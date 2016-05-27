5136b970fb19342669771b253af687d9c7b15788
dominikus1993 <dominikus1910@gmail.com>
2016-05-27 23:22:39 +0200

    add my view page adapter

---
+++ app/src/main/kotlin/utils/AstroWeatherViewPageAdapter.kt
@@ -0,0 +1,32 @@
+package utils
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import java.util.*
+
+/**
+ * Created by domin_000 on 27.05.2016.
+ */
+class AstroWeatherViewPageAdapter(val manager: FragmentManager) : FragmentPagerAdapter (manager){
+    private val  mFragmentList: ArrayList<Fragment> = arrayListOf()
+    private val mFragmentTitleList:ArrayList<String> = arrayListOf()
+
+    override fun getItem(position: Int): Fragment? {
+        return mFragmentList[position]
+    }
+
+    override fun getCount(): Int {
+        return mFragmentList.size
+    }
+
+    fun addFragment(fragment:Fragment, title:String){
+        mFragmentList.add(fragment)
+        mFragmentTitleList.add(title)
+    }
+
+    override fun getPageTitle(position: Int):String{
+        return ""
+    }
+
+}
