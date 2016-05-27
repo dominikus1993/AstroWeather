68652900bc13cebbf844639912ccca131842800d
dominikus1993 <dominikus1910@gmail.com>
2016-05-27 22:20:02 +0200

    add AppData

---
+++ app/src/main/kotlin/model/AppData.kt
@@ -0,0 +1,9 @@
+package model
+
+import com.astrocalculator.AstroCalculator
+
+/**
+ * Created by domin_000 on 27.05.2016.
+ */
+
+data class AppData(val location: AstroCalculator.Location, val interval:Int)
