5dc79c86c1fced9cfee18f8e96cea931d7b64283
dominikus1993 <dominikus1910@gmail.com>
2016-05-27 22:21:17 +0200

    add MoonData

---
+++ app/src/main/kotlin/model/MoonData.kt
@@ -0,0 +1,8 @@
+package model
+
+import com.astrocalculator.AstroCalculator
+
+/**
+ * Created by domin_000 on 27.05.2016.
+ */
+data class MoonData(val moonTime : AstroCalculator.MoonInfo, val  location: AstroCalculator.Location)
