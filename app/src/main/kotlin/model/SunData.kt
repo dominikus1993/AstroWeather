ce22c93a01a355f3b7d15c841e12d61b6cc0043a
dominikus1993 <dominikus1910@gmail.com>
2016-05-27 22:21:48 +0200

    add SunData

---
+++ app/src/main/kotlin/model/SunData.kt
@@ -0,0 +1,8 @@
+package model
+
+import com.astrocalculator.AstroCalculator
+
+/**
+ * Created by domin_000 on 27.05.2016.
+ */
+data class SunData(val sunInfo: AstroCalculator.SunInfo, val  location: AstroCalculator.Location)
