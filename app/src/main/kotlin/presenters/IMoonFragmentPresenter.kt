db55e60d8ba0c2643e0dce5afa0ad58c65f1d651
dominikus1993 <dominikus1910@gmail.com>
2016-05-27 23:04:54 +0200

    add moon presenter implementation

--- app/src/main/kotlin/presenters/IMoonFragmentPresenter.kt
+++ app/src/main/kotlin/presenters/IMoonFragmentPresenter.kt
@@ -1,8 +1,11 @@
 package presenters
 
+import android.os.Handler
+import model.AppData
+
 /**
  * Created by domin_000 on 27.05.2016.
  */
 interface IMoonFragmentPresenter{
-
+    fun moonDataTimer(handler: Handler, appSettings: AppData): Runnable
 }
--- app/src/main/kotlin/presenters/MoonFragmentPresenter.kt
+++ app/src/main/kotlin/presenters/MoonFragmentPresenter.kt
@@ -1,8 +1,28 @@
 package presenters
 
+import android.os.Handler
+import model.AppData
+import model.MoonData
+import utils.AstroCalculatorUtils
+import view.IAstroWeatherView
+
 /**
  * Created by domin_000 on 27.05.2016.
  */
 class MoonFragmentPresenter : IMoonFragmentPresenter{
+    private val view:IAstroWeatherView<MoonData>;
 
+    constructor(view: IAstroWeatherView<MoonData>) {
+        this.view = view
+    }
+
+
+    override fun moonDataTimer(handler: Handler, appSettings:AppData): Runnable {
+        return object : Runnable {
+            override fun run() {
+                view.showData(AstroCalculatorUtils.getMoonData(appSettings.location))
+                handler.postDelayed(this, 1000);
+            }
+        }
+    }
 }
