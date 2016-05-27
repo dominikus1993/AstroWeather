6be64afa65b955d4a770ffbbced4de71b181347d
dominikus1993 <dominikus1910@gmail.com>
2016-05-27 22:24:53 +0200

    add Time class

---
+++ app/src/main/kotlin/model/Time.kt
@@ -0,0 +1,8 @@
+package model
+
+import java.util.*
+
+/**
+ * Created by domin_000 on 27.05.2016.
+ */
+data class Time(var dateTime: Date)
--- app/src/main/kotlin/presenters/MainActivityPresenter.kt
+++ app/src/main/kotlin/presenters/MainActivityPresenter.kt
@@ -1,8 +1,16 @@
 package presenters
 
+import com.astrocalculator.AstroDateTime
+import model.Time
+import view.IAstroWeatherView
+
 /**
  * Created by domin_000 on 27.05.2016.
  */
-class MainActivityPresenter{
+class MainActivityPresenter : IAstroWeatherMainActivityPresenter{
+    private val view:IAstroWeatherView<Time>
 
+    constructor(view:IAstroWeatherView<Time>){
+        this.view = view
+    }
 }
