955b5b607f70fe53c0810e9dfb49df5be2ba295d
dominikus1993 <dominikus1910@gmail.com>
2016-05-27 22:55:04 +0200

    add COnstants and PreferencesUtils

---
+++ app/src/main/kotlin/utils/Constants.kt
@@ -0,0 +1,11 @@
+package utils
+
+/**
+ * Created by domin_000 on 27.05.2016.
+ */
+public enum class Constants(val value:String){
+    Location("Location"),
+    Longitude("longitude"),
+    Latitude("latitude"),
+    Interval("interval")
+}
---
+++ app/src/main/kotlin/utils/PreferencesUtils.kt
@@ -0,0 +1,37 @@
+package utils
+
+import android.content.SharedPreferences
+import com.astrocalculator.AstroCalculator
+import model.AppData
+
+/**
+ * Created by domin_000 on 27.05.2016.
+ */
+class PreferencesUtils{
+    companion object{
+        fun getPreferences(getSharedPreferences : (String, Int) -> SharedPreferences): AppData {
+            val locationPreferences = getSharedPreferences(Constants.Location.value, 0)
+            val intervalPreferences = getSharedPreferences(Constants.Interval.value, 0)
+
+            val longitude = locationPreferences.getFloat(Constants.Longitude.value, 0F).toDouble()
+            val latitude = locationPreferences.getFloat(Constants.Latitude.value, 0F).toDouble()
+
+            val location = AstroCalculator.Location(latitude, longitude)
+            val interval = intervalPreferences.getInt(Constants.Interval.value, 1)
+            return AppData(location, interval)
+        }
+    }
+
+    fun setPreferences(getSharedPreferences : (String, Int) -> SharedPreferences, appData: AppData) {
+        val locationPreferencesEditor = getSharedPreferences(Constants.Location.value, 0).edit()
+        val intervalPreferencesEditor = getSharedPreferences(Constants.Interval.value, 0).edit()
+
+        locationPreferencesEditor.putFloat(Constants.Latitude.value, appData.location.latitude.toFloat())
+        locationPreferencesEditor.putFloat(Constants.Longitude.value, appData.location.longitude.toFloat())
+        locationPreferencesEditor.commit()
+
+        intervalPreferencesEditor.putInt(Constants.Interval.value, appData.interval)
+        intervalPreferencesEditor.commit()
+
+    }
+}
