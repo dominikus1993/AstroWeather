e7d0dc7c0a2d7e70664082d709d211966116a005
dominikus1993 <dominikus1910@gmail.com>
2016-05-28 00:01:26 +0200

    add sun fragment presenter

---
+++ app/src/main/kotlin/presenters/SunFragmentPresenter.kt
@@ -0,0 +1,29 @@
+package presenters
+
+import android.os.Handler
+import model.AppData
+import model.SunData
+import utils.AstroCalculatorUtils
+import view.IAstroWeatherView
+
+/**
+ * Created by domin_000 on 27.05.2016.
+ */
+class SunFragmentPresenter{
+    private val view: IAstroWeatherView<SunData>;
+
+    constructor(view: IAstroWeatherView<SunData>) {
+        this.view = view
+    }
+
+    fun sunDataTimer(handler: Handler, appSettings: AppData): Runnable {
+        return object : Runnable {
+            override fun run() {
+
+                view.showData(AstroCalculatorUtils.getSunData(appSettings.location))
+
+                handler.postDelayed(this, appSettings.interval.toLong());
+            }
+        }
+    }
+}
