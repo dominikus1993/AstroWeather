618953e7350dc2bd3a3daeb2626ec1c436b39261
dominikus1993 <dominikus1910@gmail.com>
2016-05-31 20:15:53 +0200

    small changes

--- app/src/main/kotlin/model/AppData.kt
+++ app/src/main/kotlin/model/AppData.kt
@@ -6,4 +6,4 @@ import com.astrocalculator.AstroCalculator
  * Created by domin_000 on 27.05.2016.
  */
 
-data class AppData(val location: AstroCalculator.Location, val interval:Int, val weatherData:LocalizationWeatherData)
+data class AppData(val location: AstroCalculator.Location, val interval:Int, val weatherData:WeatherSettingsData)
